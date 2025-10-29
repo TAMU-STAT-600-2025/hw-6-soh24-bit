91e71a05e2812e34c681ea48073696bcf318f61a
Leo Oh <soh24@tamu.edu>
2025-10-29 18:22:08 -0500

    Add comments to make functions more clear

--- LassoInC.cpp
+++ LassoInC.cpp
@@ -13,9 +13,13 @@ double soft_c(double a, double lambda){
 // [[Rcpp::export]]
 double lasso_c(const arma::mat& Xtilde, const arma::colvec& Ytilde, const arma::colvec& beta, double lambda){
   // Your function code goes here
+  
   int n = Xtilde.n_rows;
+  
+  // Define Residual
   arma::colvec r = Ytilde - Xtilde * beta;
   
+  // Same objective function as in R version
   double fobj = (1.0 / (2.0 * n)) * arma::dot(r, r) + lambda * arma::accu(arma::abs(beta));
   
   return fobj;
@@ -25,23 +29,30 @@ double lasso_c(const arma::mat& Xtilde, const arma::colvec& Ytilde, const arma::
 // [[Rcpp::export]]
 arma::colvec fitLASSOstandardized_c(const arma::mat& Xtilde, const arma::colvec& Ytilde, double lambda, const arma::colvec& beta_start, double eps = 0.001){
   // Your function code goes here
+  
   int n = Xtilde.n_rows;
   int p = Xtilde.n_cols;
   
+  // Initialize beta and current objective value
   arma::colvec beta = beta_start;
   double fcurrent = lasso_c(Xtilde, Ytilde, beta, lambda);
   
   arma::colvec resid = Ytilde - Xtilde * beta;
   
   while (true) {
+    
     double fprevious = fcurrent;
     
     for (int i = 0; i < p; i++) {
+      
+      // Define old beta
       double beta_old_i = beta(i);
       
+      // Update a and Beta
       double a = (1.0 / n) * arma::dot(Xtilde.col(i), resid) + beta_old_i;
       beta(i) = soft_c(a, lambda);
       
+      // Update resid
       if (beta(i) != beta_old_i) {
         resid -= Xtilde.col(i) * (beta(i) - beta_old_i);
       }
@@ -49,6 +60,7 @@ arma::colvec fitLASSOstandardized_c(const arma::mat& Xtilde, const arma::colvec&
     
     fcurrent = lasso_c(Xtilde, Ytilde, beta, lambda);
     
+    // Check if we have reached the optimum
     if (std::abs(fprevious - fcurrent) < eps) {
       break;
     }
@@ -65,15 +77,19 @@ arma::colvec fitLASSOstandardized_c(const arma::mat& Xtilde, const arma::colvec&
 // [[Rcpp::export]]
 arma::mat fitLASSOstandardized_seq_c(const arma::mat& Xtilde, const arma::colvec& Ytilde, const arma::colvec& lambda_seq, double eps = 0.001){
   // Your function code goes here
+  
   int p = Xtilde.n_cols;
   int n_lambda = lambda_seq.n_elem;
   
+  // Initialize a beta matrix
   arma::mat beta_mat(p, n_lambda);
   
+  // Warm start
   arma::colvec beta_current(p, arma::fill::zeros);
   
   for (int i = 0; i < n_lambda; i++) {
     
+    // Update beta_mat with beta_matrix with corresponding lambda values
     arma::colvec currentfit = fitLASSOstandardized_c(Xtilde, Ytilde, lambda_seq(i), beta_current, eps);
     beta_mat.col(i) = currentfit;
     
